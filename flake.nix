0622d03121f4fda4dcc94825b7464646175e68d8
Przemek <przempore@gmail.com>
2023-12-26 23:41:13 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs { inherit system; };
+        native_client = builtins.fetchurl {
+          url = "https://github.com/belaviyo/native-client/releases/download/0.4.7/linux.zip";
+          sha256 = "sha256:12w9j4hh9zbgr6vhh88wxzdv1ppdlqy74yvv3sf3n7xf8n9jrwm5";
+        };
+        install-native-client = pkgs.writeShellScriptBin "install-native-client" ''
+          unzip ${native_client} -d $1
+          cd app
+          node install.js
+        '';
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ unzip nodejs-slim install-native-client ];
+          shellHook = ''
+            echo "Call install-native-client to install native-client";
+          '';
+        };
+    });
+}
